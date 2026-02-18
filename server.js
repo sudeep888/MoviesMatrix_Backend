7fccf94a0d8807d853d010a25b9b5d01d08eb078
sudeep888 <sudeepdas0711@gmail.com>
2026-02-19 01:55:45 +0530

    Update allowed origins for CORS configuration

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://moviesmatrix-backend.onrender.com"
+  "https://movies-matrix-frontend.vercel.app"
 ];
 
 app.use(
